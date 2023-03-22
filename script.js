09115332e52f98af7dd31ea684b9928363811c16
Gigi <106762871+gigi1189@users.noreply.github.com>
2023-03-22 19:55:29 +0000

    added pictures

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ function buzzfeedQuiz() {
   
   let button = document.querySelector(".submit");
   let displayResult = document.querySelector(".displayResult");
-  let q1 = document.querySelector(".q1");
-  let q2 = document.querySelector(".q2");
-  let q3 = document.querySelector(".q3");
-  let q4 = document.querySelector(".q4");
 
   // task 5: declare variables for values we'll set later. You'll need one for an image source, a result and a score for each question.
 button.onclick = function() {
@@ -21,75 +17,29 @@ button.onclick = function() {
   let q4Score;
 
     // task 6: Declare user input variables for each of your questions. If you have three questions, you should have three variables. These variables should equal the value of the user input for each question.
-
-  let answer1 = document.querySelector(".q1").value;
-  let answer2 = document.querySelector(".q2").value;
-  let answer3 = document.querySelector(".q3").value;
-  let answer4 = document.querySelector(".q4").value;
-    let counter = 0
     // task 7: create a conditional statement for the responses to Question 1 based on the user input.
-
- if (answer1 === "Reading") {
-   counter =  counter + 1 
- }
-  else if (answer1 === "Baking") {
-    counter = counter + 2
-  }
-    else if (answer1 === "Partying"){
-        counter = counter + 3
-      } 
-      else {
-          counter = counter + 4
-        } 
- 
-
     // task 8: create a conditional statement for the responses to Question 2 based on the user input.
-
-if (answer2 === "Curious") {
-   counter =  counter + 1 
- }
-  else if (answer2 === "Motivated") {
-    counter = counter + 2
-  }
-    else if (answer2 === "Adventurous"){
-        counter = counter + 3
-      } 
-      else {
-          counter = counter + 4
-        } 
-
-
     // task 9: create a conditional statement for the responses to Question 3 based on the user input.
-
-  if (answer3 === "Family") {
-   counter =  counter + 1 
- }
-  else if (answer3 === "Friends") {
-    counter = counter + 2
-  }
-    else if (answer3 === "Co-workers"){
-        counter = counter + 3
-      } 
-      else {
-          counter = counter + 4
-        } 
-
   // question 4 condintionals
-
-if (answer4 === "Banana") {
-   counter =  counter + 1 
- }
-  else if (answer4 === "Pear") {
-    counter = counter + 2
-  }
-    else if (answer4 === "Carrot"){
-        counter = counter + 3
-      } 
-      else {
-          counter = counter + 4
-        } 
-
     // task 10: declare a variable for the total score of the quiz and set it equal to the sum of each question score.
+  if (counter >= 4 && counter <= 5 ){
+  displayResult.innerHTML = "You are the 2014 Enesco Home Grown Banana Dachshund!" + '<img src="https://i.imgur.com/g7Qrwv2.jpg" class="resultPic">'
+}
+  else if (counter >= 6 && counter <= 8){
+    displayResult.innerHTML = "You are the 2009 Enesco Home Grown Pear Hen Chick!" + '<img src= "https://i.imgur.com/xqWKTUs.jpg" class="resultPic">'
+  }
+  else if (counter >= 9 && counter <= 10){
+    displayResult.innerHTML = "You are the 2011 Enesco Home Grown Carrot Goldfish!" + '<img src= "https://i.imgur.com/xTqSwkH.jpg" class="resultPic">'
+  }
+  else if (counter >=11 && counter <=12){
+    displayResult.innerHTML = "You are the (RARE) 2004 Enesco Home Grown Cauliflower Sheep!" + '<img src="https://i.imgur.com/S8lErdr.jpg" class="resultPic">'
+  }
+  else if (counter >=13 && counter <=15){
+    displayResult.innerHTML = "You are the 2010 Enesco Home Grown Pineapple Porcupine!" + '<img src="https://i.imgur.com/tM0QJ4I.jpg" class="resultPic">'
+  }
+  else {
+    displayResult.innerHTML = "You are the (RARE) 2011 Enesco Home Grown Cabbage Elephant!" + '<img src="https://i.imgur.com/EvMZYHH.jpg"class="resultPic">'
+  }
 
 
  //see line 29 :)
@@ -97,25 +47,90 @@ if (answer4 === "Banana") {
 
     // task 11: create a conditional statement for the result to the quiz based on the total score. Using string interpolation, display these to the page: the result, an image that corresponds and a message to go with it.
 
-if (counter >= 4 && counter <= 5 ){
-  displayResult.innerHTML = "You are the 2014 Enesco Home Grown Banana Dachshund!" + '<img src="https://i.imgur.com/g7Qrwv2.jpg">'
-}
-  else if (counter >= 6 && counter <= 8){
-    displayResult.innerHTML = "You are the 2009 Enesco Home Grown Pear Hen Chick!" + '<img src= "https://i.imgur.com/xqWKTUs.jpg">'
-  }
-  else if (counter >= 9 && counter <= 10){
-    displayResult.innerHTML = "You are the 2011 Enesco Home Grown Carrot Goldfish!" + '<img src= "https://i.imgur.com/xTqSwkH.jpg">'
-  }
-  else if (counter >=11 && counter <=12){
-    displayResult.innerHTML = "You are the (RARE) 2004 Enesco Home Grown Cauliflower Sheep!" + '<img src="https://i.imgur.com/S8lErdr.jpg">'
-  }
-  else if (counter >=13 && counter <=15){
-    displayResult.innerHTML = "You are the 2010 Enesco Home Grown Pineapple Porcupine!" + '<img src="https://i.imgur.com/tM0QJ4I.jpg">'
-  }
-  else {
-    displayResult.innerHTML = "You are the (RARE) 2011 Enesco Home Grown Cabbage Elephant!" + '<img src="https://i.imgur.com/EvMZYHH.jpg">'
-  }
 
 
   };
-}
+} 
+
+let answerPic1List = ["readingPic" , "curiousPic" , "familyPic" , "bananaPic"];
+let answerPic2List = ["bakingPic" , "motivatedPic" , "friendsPic" , "pearPic"];
+let answerPic3List = ["partyingPic" , "adventurousPic" , "coworkersPic" , "carrotPic"];
+let answerPic4List = ["instrumentPic" , "relaxedPic" , "myselfPic" , "lettucePic"];
+
+answerPic1List[0] = document.querySelector(".readingPic");
+answerPic1List[1] = document.querySelector(".curiousPic");
+answerPic1List[2] = document.querySelector(".familyPic");
+answerPic1List[3] = document.querySelector(".bananaPic");
+answerPic2List[0] = document.querySelector(".bakingPic");
+answerPic2List[1] = document.querySelector(".motivatedPic");
+answerPic2List[2] = document.querySelector(".friendsPic");
+answerPic2List[3] = document.querySelector(".pearPic");
+answerPic3List[0] = document.querySelector(".partyingPic");
+answerPic3List[1] = document.querySelector(".adventurousPic");
+answerPic3List[2] = document.querySelector(".coworkersPic");
+answerPic3List[3] = document.querySelector(".carrotPic");
+answerPic4List[0] = document.querySelector(".instrumentPic");
+answerPic4List[1] = document.querySelector(".relaxedPic");
+answerPic4List[2] = document.querySelector(".myselfPic");
+answerPic4List[3] = document.querySelector(".lettucePic");
+
+let counter = 0
+
+answerPic1List.forEach(pic => {
+  pic.onmouseover = function() {
+    if (pic.style.border !== "4px solid red") {
+  pic.style.border = "4px solid #bfbfbf" }
+};
+ pic.onmouseout = function(){
+   if (pic.style.border !== "4px solid red"){
+  pic.style.border = "none" } 
+};
+pic.onclick = function(){ 
+counter = counter + 1
+pic.style.border = "4px solid red" };
+});
+
+answerPic2List.forEach(pic => {
+  pic.onmouseover = function() {
+    if (pic.style.border !== "4px solid red") {
+  pic.style.border = "4px solid #bfbfbf" }
+};
+ pic.onmouseout = function(){
+   if (pic.style.border !== "4px solid red"){
+  pic.style.border = "none" } 
+};
+pic.onclick = function(){ 
+counter = counter + 2
+pic.style.border = "4px solid red" };
+});
+
+answerPic3List.forEach(pic => {
+  pic.onmouseover = function() {
+    if (pic.style.border !== "4px solid red") {
+  pic.style.border = "4px solid #bfbfbf" }
+};
+ pic.onmouseout = function(){
+   if (pic.style.border !== "4px solid red"){
+  pic.style.border = "none" } 
+};
+pic.onclick = function(){ 
+counter = counter + 3
+pic.style.border = "4px solid red" };
+});
+
+answerPic4List.forEach(pic => {
+  pic.onmouseover = function() {
+    if (pic.style.border !== "4px solid red") {
+  pic.style.border = "4px solid #bfbfbf" }
+};
+ pic.onmouseout = function(){
+   if (pic.style.border !== "4px solid red"){
+  pic.style.border = "none" } 
+};
+pic.onclick = function(){ 
+counter = counter + 4
+pic.style.border = "4px solid red" };
+});
+console.log(counter);
+
+
